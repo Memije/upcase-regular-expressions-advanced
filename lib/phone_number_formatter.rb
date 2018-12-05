d03c83a9e0b85ec87d07e0bcbfecf8cc7d6fab9c
Daniel Memije <daniel.memije@conekta.com>
2018-12-05 11:41:20 -0600

    Last Exercise

--- lib/phone_number_formatter.rb
+++ lib/phone_number_formatter.rb
@@ -4,6 +4,7 @@ class PhoneNumberFormatter
   end
 
   def format_number
-    @number
+    regex = /(?<international>\+\d\s)*(\W*|x)(?<area_code>\d{3})(\W*|x)(?<exchange>\d{3})(\W*|x)(?<subscriber>\d{4})(\W*|x)/
+    @number.gsub(regex, '\k<international>(\k<area_code>) \k<exchange>-\k<subscriber>')
   end
 end
